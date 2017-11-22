2140f2b92360eb97f9a740cdee81299c3496088c
SergiyMVRV <sergiy.mavrov@gmail.com>
2017-11-22 12:52:06 +0200

    init
    init

---
+++ js/all.js
@@ -0,0 +1,5 @@
+document.querySelector('.section-video').style.maxWidth = window.innerWidth;
+
+
+//document.querySelector('.section-video').style.maxWidth = 200 + 'px';
+//document.querySelector('.section-video').style.display = 'none';
